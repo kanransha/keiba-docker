16c9ac15bdc0d8f7cf5a6e869ae06080b00c2b6a
kanransha <kanransya.iqqb_io_o4@icloud.com>
2020-01-24 04:07:49 +0900

    Change jupyter to detached mode

--- shell/jupyter.sh
+++ shell/jupyter.sh
@@ -1,2 +1,2 @@
 docker run -v $HOME/Documents/keiba/learning:/tf/my-project \
-  --name jupyter -p 8000:8888 --gpus all kanransha/jupyter
+  --name jupyter -d -p 8000:8888 --gpus all kanransha/jupyter
