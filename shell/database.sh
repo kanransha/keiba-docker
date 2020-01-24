9cd92ea8a9f347815bf9e012f6ab48c558f1ddea
kanransha <kanransya.iqqb_io_o4@icloud.com>
2020-01-24 20:45:35 +0900

    Change network

--- shell/database.sh
+++ shell/database.sh
@@ -1,3 +1,2 @@
-docker run -v database:/var/lib/mysql \
-  -e MYSQL_ROOT_PASSWORD=$MYSQL_ROOT_PASSWORD \
-  --network sql-network --name database --expose 3306 -d kanransha/database
+docker run -v database:/var/lib/mysql -e MYSQL_ROOT_PASSWORD \
+  --net=host --name database -d kanransha/database
--- shell/scraping.sh
+++ shell/scraping.sh
@@ -1,4 +1,3 @@
 docker run -v $HOME/Documents/keiba/scraping:/go/src/github.com/kanransha/scraping:ro \
-   -v $HOME/Documents/keiba/htmldata/:/htmldata:ro -d \
-   --network sql-network -e MYSQL_ROOT_PASSWORD  --name scraping\
+   -v $HOME/Documents/keiba/htmldata/:/htmldata:ro -d -e MYSQL_ROOT_PASSWORD  --name scraping \
    kanransha/scraping bash -c "go build /go/src/github.com/kanransha/scraping/main.go && ./main"
